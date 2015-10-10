cbcdd6f58548e001557709c4649f6826341e04ed
azharisalleh <azhari.salleh@gmail.com>
2015-10-10 22:06:44 +0800

    Test 2 New File Get NIC Info
    Ali has been assign by Company A and Company B to develop  a SOFTWARE
    that get an information from  NIC such as MAC address

---
+++ test2companyA.java
@@ -0,0 +1,58 @@
+package Q1;
+
+/*
+ * This program for Company A:
+ * Reading information from Network Interface Card (NIC)
+ * Information : IP and MAC Address
+*/
+
+/*
+ * author : Azhari Hj Salleh
+ * matrix no : 201461166
+ * email : azhari.salleh@gmail.com
+ * github acct : https://github.com/azharisalleh
+ * 
+ */
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+
+public class test2companyA{
+	
+   public static void main(String[] args){
+		
+	InetAddress ip;
+	try {
+		
+		System.out.println("Reading Local IP and MAC Address For Company A");
+		ip = InetAddress.getLocalHost();
+		System.out.println("Current IP address : " + ip.getHostAddress());
+		
+		NetworkInterface network = NetworkInterface.getByInetAddress(ip);
+			
+		byte[] mac = network.getHardwareAddress();
+			
+		System.out.print("Current MAC address : ");
+			
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < mac.length; i++) {
+			sb.append(String.format("%02X%s", mac[i], (i < mac.length - 1) ? "-" : ""));		
+		}
+		System.out.println(sb.toString());
+			
+	} catch (UnknownHostException e) {
+		
+		e.printStackTrace();
+		
+	} catch (SocketException e){
+			
+		e.printStackTrace();
+			
+	}
+	    
+   }
+
+}
