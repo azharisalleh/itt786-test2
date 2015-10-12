61bed3a5761e9eba669db4b49f228c004b552e87
azharisalleh <azhari.salleh@gmail.com>
2015-10-12 21:27:01 +0800

    Revert "New Features Thread and IPv6 For Company A"
    This reverts commit 284d880460782fae470809499cd9ddd3ea560031.

--- test2companyA.java
+++ test2companyA.java
@@ -1,15 +1,11 @@
 package Q1;
 
-import java.io.*;
-import java.net.*;
-import java.util.*;
-import static java.lang.System.out;
-
 /*
  * This program for Company A:
- * New Enhancement  features to get information about IPV6 and Thread 
- * Information : IPV6, MAC Address and Thread.
+ * Enhancement  features to get information about IPV6 
+ * Information : IPV6 and MAC Address
 */
+
 /*
  * author : Azhari Hj Salleh
  * matrix no : 201461166
@@ -18,69 +14,43 @@ import static java.lang.System.out;
  * 
  */
 
-class MyClasss implements Runnable{
+import java.net.Inet6Address;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import static java.lang.System.out;
 
-	@Override
-	public void run() {
-		
-		Enumeration<NetworkInterface> nets = null;
-		try {
-			nets = NetworkInterface.getNetworkInterfaces();
-		} catch (SocketException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
+
+public class test2companyA{
+	
+    public static void main(String args[]) throws SocketException {
+        Enumeration<NetworkInterface> nets = NetworkInterface.getNetworkInterfaces();
         for (NetworkInterface netint : Collections.list(nets))
             displayInterfaceInformation(netint);
-		
-	try {
-		//Pause each thread by 100milisecond
-		Thread.sleep(100);
-	} catch (InterruptedException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
-		
-	}
+    }
 
-	private void displayInterfaceInformation(NetworkInterface netint) {
-		// TODO Auto-generated method stub
+    static void displayInterfaceInformation(NetworkInterface netint) throws SocketException {
+       
         out.printf("\n");
         System.out.println("Initial Program to Access of IPv6 of Subinterface");
-    	out.printf(Thread.currentThread().getId()+ " Display name: %s\n", netint.getDisplayName());
-        out.printf(Thread.currentThread().getId()+ " Name: %s\n", netint.getName());
+    	out.printf("Display name: %s\n", netint.getDisplayName());
+        out.printf("Name: %s\n", netint.getName());
         Enumeration<InetAddress> inetAddresses = netint.getInetAddresses();
         
         for (InetAddress inetAddress : Collections.list(inetAddresses)) {
-            out.printf(Thread.currentThread().getId()+ " InetAddress: %s\n", inetAddress);
+            out.printf("InetAddress: %s\n", inetAddress);
         }
        
 
-        try {
-			out.printf(Thread.currentThread().getId()+ " Hardware address: %s\n",
-			            Arrays.toString(netint.getHardwareAddress()));
-		} catch (SocketException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+        out.printf("Hardware address: %s\n",
+                    Arrays.toString(netint.getHardwareAddress()));
         
         out.printf("\n");
         System.out.println("End Info for Subinterface");
-	}
-	
-}
-public class test2companyA {
-
-	public static void main(String[] args) throws SocketException {
-		
-		
-		Thread t1 = new Thread(new MyClasss());
-		Thread t2 = new Thread(new MyClasss());
-		Thread t3 = new Thread(new MyClasss());
-		t1.start();
-		t2.start();
-		t3.start();
-	}
+     
+	    
+   }
 
 }
-
